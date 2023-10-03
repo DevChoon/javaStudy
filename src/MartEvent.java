5d724f086a0722b33e59347c49c79cf3dc33ca0f
pomeranian91 <forking91@gmail.com>
2023-10-03 19:55:51 +0900

    study

---
+++ src/MartEvent.java
@@ -0,0 +1,5 @@
+public class MartEvent {
+  public static void main(String[] args) {
+    int ration = 15;
+  }
+}
---
+++ src/StringExample.java
@@ -0,0 +1,9 @@
+public class StringExample {
+  public static void main(String[] args) {
+    System.out.println("나는 문자열");
+    System.out.println('나');
+
+    System.out.println("나는 \"자바\"야");
+    System.out.println("치킨 \n 만세!");
+  }
+}
